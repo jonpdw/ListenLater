b1af90a5524a25537ba429790217d1173eae0b65
Jonathan <jonpdw@gmail.com>
2020-10-12 03:29:46 +1300

    Added Runtime Compilation to Razor. e.g. I can live update Razor pages

--- APITest/Startup.cs
+++ APITest/Startup.cs
@@ -27,6 +27,9 @@ namespace APITest
         {
             services.AddControllers();
             services.AddRazorPages();
+            services
+                .AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
