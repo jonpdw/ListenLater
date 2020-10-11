c2a022145970414301314d9570e2518435aeb2b5
Jonathan <jonpdw@gmail.com>
2020-10-11 20:28:40 +1300

    Initial commit

---
+++ APITest/Controllers/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+namespace APITest.Controllers {
+    [ApiController]
+    [Route("api/go")]
+    public class StartProcessController : ControllerBase {
+        
+        private readonly IHostEnvironment _hostingEnvironment;
+        private readonly IBackgroundTaskQueue _taskQueue;
+        private readonly ILogger<StartProcessController> _logger;
+
+        public StartProcessController(IHostEnvironment hostingEnvironment, IBackgroundTaskQueue taskQueue, ILogger<StartProcessController> logger) {
+            _hostingEnvironment = hostingEnvironment;
+            _taskQueue = taskQueue;
+            _logger = logger;
+        }
+
+        [Route("{username}")]
+        public string Get(string username) {
+            _logger.LogInformation(username);
+            _logger.LogInformation("---Get Request---");
+            string projectRootPath = _hostingEnvironment.ContentRootPath;
+            _taskQueue.QueueBackgroundWorkItem(async cancelToken => {
+                await DownloadYouTube.Do(projectRootPath, cancelToken, _logger);
+            });
+            return "Done";
+        }
+            
+    };
+}
---
+++ APITest/DownloadYouTube.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using APITest.Controllers;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using YoutubeDLSharp;
+using YoutubeDLSharp.Options;
+using static APITest.UploadFileToOvercastClass;
+
+
+namespace APITest {
+    public class DownloadYouTube {
+        public static async Task Do(string projectRootPath, CancellationToken cancelToken,
+            ILogger<StartProcessController> logger) {
+            var optionsGetPlaylist = new OptionSet {
+                Cookies = projectRootPath + "/youtube_cookies.txt",
+                DumpJson = true,
+                FlatPlaylist = true,
+                PlaylistEnd = 3,
+                DownloadArchive = projectRootPath + "/archive.txt",
+            };
+
+            var optionsGetFileName = new OptionSet {
+                Output = "mp3/%(uploader)s - %(title)s.%(ext)s",
+                Format = "worstaudio[ext=m4a]",
+                GetFilename = true,
+                NoProgress = true,
+            };
+
+            var optionsDownloadAudio = new OptionSet {
+                Output = "mp3/%(uploader)s - %(title)s.%(ext)s",
+                Format = "worstaudio[ext=m4a]",
+            };
+
+            var ytdl = new YoutubeDL();
+            // set the path of the youtube-dl and FFmpeg if they're not in PATH or current directory
+            ytdl.YoutubeDLPath = "youtube-dl";
+            // ytdl.FFmpegPath = "/usr/local/bin/ffmpeg";
+
+            var watchLaterVideos = await ytdl.RunWithOptions(
+                new[] {"https://www.youtube.com/playlist?disable_polymer=true&list=WL"}, optionsGetPlaylist,
+                cancelToken);
+            
+            if (watchLaterVideos.Success == false) {
+                Console.WriteLine("Problem getting watch later playlist");
+                return;
+            }
+
+            logger.LogInformation($"Watch later has a length of {watchLaterVideos.Data.Length}");
+            
+            foreach (var videoString in watchLaterVideos.Data) {
+                cancelToken.ThrowIfCancellationRequested();
+                var videoDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(videoString);
+                logger.LogInformation(videoDict["title"]);
+                var videoName =
+                    ytdl.RunWithOptions(new[] {videoDict["url"]}, optionsGetFileName, new CancellationToken());
+                var videoDownload = await ytdl.RunWithOptions(new[] {videoDict["url"]}, optionsDownloadAudio,
+                    cancelToken);
+                await videoName;
+                var fileName = videoName.Result.Data[0];
+                await UploadFileToOvercast(fileName);
+                File.AppendAllText(projectRootPath + "/archive.txt", $"youtube {videoDict["url"]}" + Environment.NewLine);
+                File.Delete(fileName);
+                
+            }
+        }
+    }
+}
