329562c0c44a18bd71d560467c1f7087e159e96b
Jonathan <jonpdw@gmail.com>
2020-10-13 00:02:46 +1300

    Now tracking if the user wants to run this every 30 minutes in a text file

--- ListenLater/Pages/Setup.cshtml.cs
+++ ListenLater/Pages/Setup.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -7,6 +8,7 @@ using Newtonsoft.Json;
 namespace ListenLater.Pages {
     [BindProperties]
     public class AddDetails : PageModel {
+        public Boolean runEvery30Min { get; set; }
         public string username { get; set; }
         public string overcast_username { get; set; }
 
@@ -21,7 +23,18 @@ namespace ListenLater.Pages {
             Console.WriteLine("Posted");
             UpdateJSON();
             System.IO.File.WriteAllText($"user-data/cookies/{username}.txt", youtube_cookies); 
+            AddToRunEvery30MinsFile();
             return RedirectToPage();
+        }
+
+        private void AddToRunEvery30MinsFile() {
+            if (runEvery30Min) {
+                HashSet<string> lines = System.IO.File.ReadLines("user-data/RunEvery30Mins.txt").ToHashSet();
+                lines.Add(username);
+                System.IO.File.WriteAllText("user-data/RunEvery30Mins.txt", string.Join("\n", lines.ToArray()));
+            }
+            
+            
         }
 
         private void UpdateJSON() {
@@ -41,7 +54,7 @@ namespace ListenLater.Pages {
                     new Dictionary<string, string> {
                         {"user", pushover_user}
                     }
-                }
+                },
             };
             string json = JsonConvert.SerializeObject(userDet, Formatting.Indented);
             System.IO.File.WriteAllText("user-data/UserDetails.json", json);
