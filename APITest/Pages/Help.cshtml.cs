3eb23edea2f935411f877d56626e0c805fe8d2ff
Jonathan <jonpdw@gmail.com>
2020-10-12 22:16:16 +1300

    Renamed AddDetails to Setup --- Added help page

---
+++ APITest/Pages/Help.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APITest.Pages {
+    public class Instructions : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
